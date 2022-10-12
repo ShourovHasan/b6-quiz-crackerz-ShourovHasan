3d82591988651a21ba3b7612b4692dcfa34d7557
ShourovHasan <shourovhasan@gmail.com>
2022-10-12 17:20:59 +0600

    blog first question added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -4,7 +4,15 @@ import { Accordion } from 'react-bootstrap';
 const Blog = () => {
     return (
         <div className='w-75 m-auto pt-5'>
-            
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>What is the purpose of the react router?</Accordion.Header>
+                    <Accordion.Body>
+                        A package called React Router aids developers in organizing and navigating various views in a React application. The React Router maintains the UI constant across various screen sizes and device kinds while making it simple to specify and render many routes in an application.
+                    </Accordion.Body>
+                </Accordion.Item>
+                
+            </Accordion>
         </div>
     );
 };
