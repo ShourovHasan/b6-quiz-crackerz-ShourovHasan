8a91527ed4b851f412da591d096a57b17bc7d9e5
ShourovHasan <shourovhasan@gmail.com>
2022-10-12 17:21:33 +0600

    blog second question added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -11,6 +11,12 @@ const Blog = () => {
                         A package called React Router aids developers in organizing and navigating various views in a React application. The React Router maintains the UI constant across various screen sizes and device kinds while making it simple to specify and render many routes in an application.
                     </Accordion.Body>
                 </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>How does the context API work in ReactJS?</Accordion.Header>
+                    <Accordion.Body>
+                        Create global variables that may be sent down a React component tree by using the React Context API. When you wish to exchange information that can be accessible by several app components without having to explicitly send props down at each level, you utilize the Context API.
+                    </Accordion.Body>
+                </Accordion.Item>
                 
             </Accordion>
         </div>
